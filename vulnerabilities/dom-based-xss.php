236d47229f028da21775f70bc5e2b6112101d116
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-11-15 09:44:21 +0300

    Add files via upload

---
+++ vulnerabilities/dom-based-xss.php
@@ -0,0 +1,11 @@
+Что выберешь ты?
+
+<select><script>
+ 
+document.write("<OPTION value=1>"+document.location.href.substring(document.location.href.indexOf("default=")+8)+"</OPTION>");
+ 
+document.write("<OPTION value=2>Кот</OPTION>");
+document.write("<OPTION value=3>Собака</OPTION>");
+document.write("<OPTION value=4>Котопес</OPTION>");
+
+</script></select>
