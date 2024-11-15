6903be48bea514069e630950430d252658ad67bd
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-11-15 09:46:02 +0300

    Update xss-html-homework.php

--- vulnerabilities/xss-html-homework.php
+++ vulnerabilities/xss-html-homework.php
@@ -1,18 +1,16 @@
 <?php
 
-echo "<b>Задание:</b> <br>- проэксплуатируйте XSS в GET-параметре xss <br>- проэксплуатируйте HTML Injection в GET-параметре htmli <br><br>";
-
 if ( isset($_GET['xss']) ) {
     $xss = str_ireplace('<script>', '', $_GET['xss']);
     $xss = str_ireplace('</script>', '', $xss);
-    echo "<b>ID страницы - ${xss}</b>";
+    echo "<b>ID page - ${xss}</b>";
 }
 
 if ( isset($_GET['htmli']) ) {
     $htmli = str_replace('<img ', '', $_GET['htmli']);
     $htmli = str_replace('</img>', '', $htmli);
     $htmli = str_replace('src', '', $htmli);
-    echo "<b>ID страницы - ${htmli}</b>";
+    echo "<b>ID page - ${htmli}</b>";
 }
 
 ?>
